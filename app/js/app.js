316ff4e765800111cb6538f242e030466d0d4e76
Andrew Reid <mrandrewreid@hotmail.com>
2014-01-10 17:51:29 +0800

    added edit page
    have added the edit page and tested some input controls

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('reactor', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'reactor.filters',
+  'reactor.services',
+  'reactor.directives',
+  'reactor.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when( '/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'Dashboard'});
+  $routeProvider.when( '/edit', {templateUrl: 'partials/edit.html', controller: 'Edit' });
+  $routeProvider.when( '/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1' });
+  $routeProvider.when( '/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2' });
+  $routeProvider.otherwise({redirectTo: '/dashboard'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,47 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module( 'reactor.controllers', [] ).
+
+  controller('Dashboard', [ '$scope' , function( $scope ) {
+
+	$scope.reactors = [
+		{type:'expose', title:'Exposé'},
+		{type:'slideshow', title:'Slideshow'},
+		{type:'quiz',title:'Quiz'}
+	] ;
+	$scope.orderProp = 'age' ;
 
   }])
-  .controller('MyCtrl2', [function() {
+ 
+  .controller('Edit', [ '$scope' , function( $scope ) {
+	$scope.reactor = { 
+		type: 'slideshow' , 
+		meta: {
+			title: 'A new slideshow' , 
+			description: 'Here is a description of this reactor.' ,
+			author: { first_name: 'Bill' , last_name: 'Bagins' , organisation: '' } ,
+			modified: 'This is a description of this reactor.' ,
+		} ,
+		entries: [
+				{ 
+					type: 'slide' , 
+					meta: { title: 'the title of slide 1' } ,
+					text: 'This is all the text of the slide and it is beautiful.'  
+				} ,
+				{ 
+					type: 'slide' , 
+					meta: { title: 'the title of slide 2' },
+					text: 'This is a bunch of text to display as the content of the slide.' 
+				}
+			]
+		} ;
+  }])
+
+  .controller('MyCtrl1', [ '$scope' , function( $scope ) {
+
+  }])
+
+  .controller('MyCtrl2', [ '$scope' , function( $scope ) {
 
   }]);
