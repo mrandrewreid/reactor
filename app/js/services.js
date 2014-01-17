71f7668b81233cc5468d988acf6ac62b870698f2
Andrew Reid <mrandrewreid@hotmail.com>
2014-01-17 22:16:03 +0800

    slight tweak to saving
    boo to the ya

--- app/js/services.js
+++ app/js/services.js
@@ -8,39 +8,19 @@ var reactor_services = angular.module( 'reactor.services', [] ) ;
 			var server = {} ;
 
 				server.saveReactor = function( reactor_data , user_data ) { 
-					//alert( 'save reactor file with id: ' + reactor_data.id + ' for user: ' + user_data.id ) ;
-					//var send_data = { u: '00001' , r: 'slideshow_01' , reactor: reactor_data , user_id: user_data.id } ;
-					var call = {
-						url: 'php/save.php' ,
-						method: 'POST' ,
-						params: { u: '00001' , r: 'slideshow_01' }
-					}; 
-					//$http( call ).success( function( data ) {
-						//console.log( 'YAY: ' +  JSON.stringify( data ) ) ;
-					//	reactor_data.meta.description.text = data ;
-					//});
-					
-					$http.post( 'php/save.php' , { reactor: reactor_data , user: user_data } ).success( function( data ) {
-						//console.log( 'YAY: ' +  JSON.stringify( data ) ) ;
-						//reactor_data.meta.description.text = data ;
-						if ( !reactor_data.meta.version ) { reactor_data.meta.version = 1 ; } else { reactor_data.meta.version++ }
-					});
-					
-					
-					
-					/*$http.get( 'php/save.php' , send_data ).success( function( data ) {
-						//console.log( 'YAY: ' +  JSON.stringify( data ) ) ;
-						reactor_data.meta.description.text = data ;
-					});*/
-					
-					
+				
+					if ( !reactor_data.meta.version ) { 
+						reactor_data.meta.version = 1 ; 
+					} else { 
+						reactor_data.meta.version++ 
+					}
 					// this is supposed to be written differently if i want it to be a restful architecture //
 					// but i just want it to work for now //
-					/*
-					$http.get( 'users/' + user_data.id + '/' + reactor_data.id + '/materials.json' ).success( function( data ) {
-						//$scope.user = data ;
-						console.log( 'YAY: ' +  JSON.stringify( data ) ) ;
-					});*/
+					$http.post( 'php/save.php?t=' + new Date() , { reactor: reactor_data , user: user_data } ).success( function( data ) {
+						console.log( data ) ;
+					});
+					
+
 				} ;
 
 				server.loadReactor = function( reactor_id , user_id ){ 
